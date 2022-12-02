02fe9ce5acc3710831b777d23c7ea6f272020fa7
Дима Ткаченко <winstooon46@gmail.comgit config --global user.email winstooon46@gmail.com>
2022-12-02 11:15:39 +0200

    Update 2022-12-02T09:15:33.075Z

--- src/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.header__burger');
-const menu = document.querySelector('.header__list');
-burger.addEventListener('click', ()=>{
-    burger.classList.toggle('active')
-    menu.classList.toggle('active')
-})
-
